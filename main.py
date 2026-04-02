e8178e10e3f72f11edca1bbf27f364ae53d16580
Sudharsan Reddy <sudharsanreddy.saragada@gmail.com>
2026-04-02 15:00:01 +0530

    health is added for server to keep live

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ Base.metadata.create_all(bind=engine)
 def home():
     return {"message": "Backend running"}
 
+# To keep Render Server alive, we can add a simple health check endpoint
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
 app.include_router(user.router)
 app.include_router(auth.router)
